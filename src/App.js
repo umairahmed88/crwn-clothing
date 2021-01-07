90805f5ed18427bdd9f8c7d266599eb0440294dd
umairahmed88 <umairahmed_88@ymail.com>
2021-01-07 14:49:13 +0500

    broken our homepage into smaller components, finished styling our menu-item components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './HomePage.Compnent';
+import HomePage from './pages/homepage/HomePage.Compnent';
 
 function App() {
   return (
